1e6d70969d8f49c8a9520f49fed24db99140349a
Jack Lu <jacklu@jacklu.me>
2017-01-17 00:34:18 +0800

    +Sprite

--- dist/angular-simple-sprite.js
+++ dist/angular-simple-sprite.js
@@ -20,109 +20,115 @@
             },
 
             link: function($scope, element, attributes) {
-
-                var src,
-                    frameWidth,
-                    frameHeight,
-                    frames,
-                    framesPerRow = 0,
-                    repeat = true,
-                    speed = 100;
-
-                // Keeps track of the current x and y positions of the sprite.
-                var spritePosition = {
-                    'x': 0,
-                    'y': 0
-                };
-
-                var scale;
-
-                /**
-                 * Initializes the sprite with default CSS styles and options passed in by
-                 * the user. Starts the sprite animation.
-                 */
-                function init() {
-                    src = $scope.src;
-                    frameWidth = parseInt($scope.frameWidth, 10);
-                    frameHeight = parseInt($scope.frameHeight, 10);
-                    frames = parseInt($scope.frames, 10);
-                    repeat = $scope.repeat == 'true';
-                    speed = $scope.speed
-                    framesPerRow = $scope.framesPerRow;
-
-                    var width = element.width();
-                    scale = width / frameWidth;
-
-                    element.css({
-                        "display": "block",
-                        "background": "url(" + src + ") repeat",
-                        "background-size": framesPerRow * 100 + '%',
-                        "backgroundPosition": "0px 0px"
-                    });
-
-                    animate();
-                }
-
-                var animationInterval = null;
-
-                /**
-                 * Animates the sprite.
-                 */
-                function animate() {
-
-                    /**
-                     * Returns whether the sprite animation has completed or not.
-                     */
-                    function isAnimationComplete() {
-                        var toReturn = false;
-
-                        if (framesPerRow) {
-                            if (spritePosition.y / frameHeight * framesPerRow + spritePosition.x / frameWidth >= frames) {
-                                toReturn = true;
-                            }
-
-                        } else {
-                            if (spritePosition.x >= frameWidth * frames) {
-                                toReturn = true;
-                            }
-                        }
-
-                        return toReturn;
-                    }
-
-                    animationInterval = $window.setInterval(function() {
-                        // Update the sprite frame
-                        element.css("background-position", -spritePosition.x * scale + "px" + " " + -spritePosition.y * scale + "px");
-
-                        // Determine if we should loop the animation, or stop, if the animation is complete
-                        if (isAnimationComplete()) {
-                            if (repeat) {
-                                spritePosition.x = 0;
-                                spritePosition.y = 0;
-                            } else {
-                                $window.clearInterval(animationInterval);
-                                // $interval.cancel(animationInterval);
-                            }
-                        } else {
-                            // Increment the X position
-                            spritePosition.x += frameWidth;
-
-                            // Check if we should move to the next row
-                            if (framesPerRow != null && spritePosition.x + frameWidth > frameWidth * framesPerRow) {
-                                spritePosition.x = 0;
-                                spritePosition.y += frameHeight;
-                            }
-                        }
-                    }, speed);
-                }
-
-                $scope.$on("$destroy", function() {
-                    $window.clearInterval(animationInterval);
-                    // $interval.cancel(animationInterval);
-                });
-
-                init();
+            	var sprite = new Sprite($scope, element);
+            	sprite.play();
             }
         };
+        function Sprite($scope, element) {
+
+            var src,
+                frameWidth,
+                frameHeight,
+                frames,
+                framesPerRow = 0,
+                repeat = true,
+                speed = 100;
+
+            // Keeps track of the current x and y positions of the sprite.
+            var spritePosition = {
+                'x': 0,
+                'y': 0
+            };
+
+            var scale;
+
+            /**
+                * Initializes the sprite with default CSS styles and options passed in by
+                * the user. Starts the sprite animation.
+                */
+            function init() {
+                src = $scope.src;
+                frameWidth = parseInt($scope.frameWidth, 10);
+                frameHeight = parseInt($scope.frameHeight, 10);
+                frames = parseInt($scope.frames, 10);
+                repeat = $scope.repeat == 'true';
+                speed = $scope.speed
+                framesPerRow = $scope.framesPerRow;
+
+                var width = element.width();
+                scale = width / frameWidth;
+
+                element.css({
+                    "display": "block",
+                    "background": "url(" + src + ") repeat",
+                    "background-size": framesPerRow * 100 + '%',
+                    "backgroundPosition": "0px 0px"
+                });
+            }
+
+            var animationInterval = null;
+
+            /**
+                * Animates the sprite.
+                */
+            function animate() {
+
+                /**
+                    * Returns whether the sprite animation has completed or not.
+                    */
+                function isAnimationComplete() {
+                    var toReturn = false;
+
+                    if (framesPerRow) {
+                        if (spritePosition.y / frameHeight * framesPerRow + spritePosition.x / frameWidth >= frames) {
+                            toReturn = true;
+                        }
+
+                    } else {
+                        if (spritePosition.x >= frameWidth * frames) {
+                            toReturn = true;
+                        }
+                    }
+
+                    return toReturn;
+                }
+
+                animationInterval = $window.setInterval(function() {
+                    // Update the sprite frame
+                    element.css("background-position", -spritePosition.x * scale + "px" + " " + -spritePosition.y * scale + "px");
+
+                    // Determine if we should loop the animation, or stop, if the animation is complete
+                    if (isAnimationComplete()) {
+                        if (repeat) {
+                            spritePosition.x = 0;
+                            spritePosition.y = 0;
+                        } else {
+                            $window.clearInterval(animationInterval);
+                            // $interval.cancel(animationInterval);
+                        }
+                    } else {
+                        // Increment the X position
+                        spritePosition.x += frameWidth;
+
+                        // Check if we should move to the next row
+                        if (framesPerRow != null && spritePosition.x + frameWidth > frameWidth * framesPerRow) {
+                            spritePosition.x = 0;
+                            spritePosition.y += frameHeight;
+                        }
+                    }
+                }, speed);
+            }
+
+            $scope.$on("$destroy", function() {
+                $window.clearInterval(animationInterval);
+                // $interval.cancel(animationInterval);
+            });
+
+            init();
+
+            return {
+            	play: animate
+            };
+        }
     }
 })(angular);
